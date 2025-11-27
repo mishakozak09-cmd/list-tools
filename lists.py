bec44a8382714e124d2d6a73502b8ee5c5789f93
Kozak Mykhailo <mishakozak09@gmail.com>
2025-11-27 19:03:54 +0200

    Initial commit: list tools project

---
+++ lists.py
@@ -0,0 +1,24 @@
+def get_even_numbers(numbers):
+    """Повертає список парних чисел."""
+    return [n for n in numbers if n % 2 == 0]
+
+
+def get_unique_sorted(numbers):
+    """Повертає відсортований список унікальних елементів."""
+    return sorted(set(numbers))
+
+
+def average(numbers):
+    """Повертає середнє значення списку."""
+    if not numbers:
+        return 0
+    return sum(numbers) / len(numbers)
+
+
+if __name__ == "__main__":
+    data = [1, 2, 2, 3, 4, 5, 5, 6]
+
+    print("Початковий список:", data)
+    print("Парні числа:", get_even_numbers(data))
+    print("Унікальні відсортовані:", get_unique_sorted(data))
+    print("Середнє значення:", average(data))
